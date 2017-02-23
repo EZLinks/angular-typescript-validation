df2ab465ba9ebe2db6537d779326d7f7de10b658
Vladislav Sudakov <sudakovmail@gmail.com>
2017-02-23 17:43:37 +0800

    minification support

--- src/utils/validationUtilities.ts
+++ src/utils/validationUtilities.ts
@@ -49,13 +49,14 @@ export class ValidationUtilities {
      */
     public static fromExpression<T>(func: (obj: T) => void): string {
 
-        let varExtractor: RegExp = new RegExp('return (.*);');
+        let varExtractor: RegExp = new RegExp('return ([^;]*)(?=(;|\\}))');
         let m: RegExpExecArray = varExtractor.exec(func + '');
 
-        if (m && m.length === 2) {
+        if (m && m.length >= 2) {
             let parts: Array<string> = m[1].split('.');
             if (parts.length === 2) {
-                return parts[1];
+                let fieldName = parts[1];
+				return fieldName.replace(' ', '');
             }
         }
 
