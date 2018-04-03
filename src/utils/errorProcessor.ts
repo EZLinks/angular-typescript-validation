f4584c487fe0dbcb4ea13aee0b2de916cea4b211
Unknown <ejohnson@ezlinks.com>
2018-04-03 14:27:05 -0400

    Upgraded angular

--- src/utils/errorProcessor.ts
+++ src/utils/errorProcessor.ts
@@ -82,7 +82,7 @@ export class ErrorProcessor {
             return false;
         }
 
-        return form.$error[fieldName].validationAttrs[attribute] === true;
+        return form.$error[fieldName].length > 0;
     }
 
     /**
